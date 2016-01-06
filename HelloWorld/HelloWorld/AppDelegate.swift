0658c6ae05c89ebe6d95da4b6aefdb0fb80447a8
zhichengzhang <zhang_zhicheng92@sina.com>
2016-01-06 15:48:16 +0800

    first commit

---
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by 章志诚 on 16/1/6.
+//  Copyright © 2016年 章志诚. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 章志诚 on 16/1/6.
+//  Copyright © 2016年 章志诚. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
